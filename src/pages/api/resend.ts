3a092feec5305c7d4f65b3d32112615fa2a7eb57
LuisDavidTorres <luisdavidtt@outlook.com>
2024-11-03 13:15:14 -0300

    added api resend error handling

--- src/pages/api/resend.ts
+++ src/pages/api/resend.ts
@@ -18,25 +18,34 @@ export const POST: APIRoute = async ({ request }) => {
     );
   }
 
-  const { data, error } = await resend.emails.send({
-    from: "Acme <onboarding@resend.dev>",
-    to: ["luisdavidtts@outlook.com"],
-    subject: "Contacto mediante portfolio",
-    html: `<strong>Nombre contacto: ${dataFormJson.name},<br> Email: ${dataFormJson.email}, <br> Mensaje: ${dataFormJson.message} </strong>`,
-  });
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: ["luisdavidtts@outlook.com"],
+      subject: "Contacto mediante portfolio",
+      html: `<strong>Nombre contacto: ${dataFormJson.name},<br> Email: ${dataFormJson.email}, <br> Mensaje: ${dataFormJson.message} </strong>`,
+    });
 
-  if (error) {
-    JSON.stringify({
-      message:
-        "Ha ocurrido un error mientras se enviava el formulario, por favor intente nuevamnete",
-    }),
-      { status: 200 };
+    if (error) {
+      JSON.stringify({
+        message:
+          "Ha ocurrido un error mientras se enviava el formulario, por favor intente nuevamnete",
+      }),
+        { status: 200 };
+    }
+
+    return new Response(
+      JSON.stringify({
+        message: "Correo enviado",
+      }),
+      { status: 200 }
+    );
+  } catch (e) {
+    return new Response(
+      JSON.stringify({
+        message: "Error al enviar correo",
+      }),
+      { status: 500 }
+    );
   }
-
-  return new Response(
-    JSON.stringify({
-      message: "Correo enviado",
-    }),
-    { status: 200 }
-  );
 };
