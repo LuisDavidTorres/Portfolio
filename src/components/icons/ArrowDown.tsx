c33017abd321d9c9e361bd08c463e0a9f12563c5
LuisDavidTorres <luisdavidtt@outlook.com>
2025-04-11 08:57:31 -0400

    add responsive Max-Width

--- src/components/icons/ArrowDown.tsx
+++ src/components/icons/ArrowDown.tsx
@@ -8,7 +8,7 @@ export default function ArrowDown() {
       height="256"
       viewBox="0 0 256 256"
       xmlSpace="preserve"
-      className="animate-bounce size-6 fill-white dark:fill-black"
+      className="animate-bounce size-6 fill-white"
     >
       <g
         style={{
@@ -26,7 +26,7 @@ export default function ArrowDown() {
       >
         <path
           d="M 90 24.25 c 0 -0.896 -0.342 -1.792 -1.025 -2.475 c -1.366 -1.367 -3.583 -1.367 -4.949 0 L 45 60.8 L 5.975 21.775 c -1.367 -1.367 -3.583 -1.367 -4.95 0 c -1.366 1.367 -1.366 3.583 0 4.95 l 41.5 41.5 c 1.366 1.367 3.583 1.367 4.949 0 l 41.5 -41.5 C 89.658 26.042 90 25.146 90 24.25 z"
-          className="fill-white dark:fill-black"
+          className="fill-white"
         />
       </g>
     </svg>
