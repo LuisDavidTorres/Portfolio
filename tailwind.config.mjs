ecb9c6a40805a580a461f0191d8b7fccffda196a
LuisDavidTorres <luisdavidtt@outlook.com>
2025-04-29 17:16:57 -0400

    add badge tecnologies in projects

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				"green-custom": "#0bff84",
-				"gray-custom": "#1b1a1b",
-				"gray-ligh": "#9f9f9f"
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "green-custom": "#0bff84",
+        "gray-custom": "#1b1a1b",
+        "gray-ligh": "#9f9f9f",
+      },
+    },
+  },
+  plugins: [],
+};
